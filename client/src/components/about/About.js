7e455c66bb797fd506b6659f0680dfd05c07442b
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-20 18:40:58 +0200

    added nav and hamburger for about page

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,9 +1,60 @@
-import React from 'react-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import about from './about.css';
 
 export const About = () => {
+  const [menyActive, setMenyActive] = useState(false);
+
   return (
     <div className='aboutContainer'>
+      {/* mobile device hamburger */}
+      <div
+        onClick={() => setMenyActive(!menyActive)}
+        className='wrapperHamburger'
+      >
+        {menyActive ? (
+          <div className='wrapperDropdown'>
+            <ul className='wrapperHamburgerRoutes'>
+              <li className='hamburgerRoutes'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='hamburgerRoutes'>
+                <Link to='/players'>Players</Link>
+              </li>
+              <li className='hamburgerRoutes'>
+                <Link to='/about'>about</Link>
+              </li>
+            </ul>
+            <div className='hamburgerIcon'>
+              <i class='fas fa-volleyball-ball'></i>{' '}
+            </div>
+          </div>
+        ) : (
+          <div className='hamburgerIcon'>
+            <i class='fas fa-volleyball-ball'></i>{' '}
+          </div>
+        )}
+      </div>
+      {/* desktop device navbar  */}
+      <div className='header'>
+        <div className='smallLogoContainer'>
+          <div className='smallLogo'></div>
+        </div>
+        <div className='nav'>
+          <ul className='wrapperDesktopRoutes'>
+            <li className='desktopRoutes'>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className='desktopRoutes'>
+              <Link to='/players'>Players</Link>
+            </li>
+            <li className='desktopRoutesAbout'>
+              <Link to='/about'>about</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className='aboutGreg'>
         <h2>About Greg</h2>
       </div>
