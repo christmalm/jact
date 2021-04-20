943c5ab1c596f01b6bb89af5edeb725b5989db5f
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-20 09:59:07 +0200

    fix hamburger meny for mobile , ipad

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,11 +1,48 @@
-import React from 'react-dom';
+import React, { useState } from 'react';
 import home from './home.css';
 
 export const Home = () => {
+  const [menyActive, setMenyActive] = useState(false);
   return (
     <div className='wrapperHomeContainer'>
-      <div className='wrapperLogoContainer'>
-        <div className='LogoContainer'></div>
+      {/* mobile device hamburger */}
+      <div
+        onClick={() => setMenyActive(!menyActive)}
+        className='wrapperHamburger'
+      >
+        {menyActive ? (
+          <div className='wrapperDropdown'>
+            <ul className='wrapperHamburgerRoutes'>
+              <li className='hamburgerRoutes'>Home</li>
+              <li className='hamburgerRoutes'>Players</li>
+              <li className='hamburgerRoutes'>About</li>
+            </ul>
+            {/* <div className='iconActive'>
+              <i class='fas fa-volleyball-ball'></i>{' '}
+            </div> */}
+          </div>
+        ) : (
+          <div className='hamburgerIcon'>
+            <i class='fas fa-volleyball-ball'></i>{' '}
+          </div>
+        )}
+      </div>
+      {/* desktop device navbar  */}
+      <div className='header'>
+        <div className='smallLogoContainer'>
+          <div className='smallLogo'></div>
+        </div>
+        <div className='nav'>
+          <ul className='wrapperDesktopRoutes'>
+            <li className='desktopRoutes'>home</li>
+            <li className='desktopRoutes'>players</li>
+            <li className='desktopRoutes'>about</li>
+          </ul>
+        </div>
+      </div>
+      {/* center Logo  */}
+      <div className='logoContainer'>
+        <div className='logo'></div>
       </div>
     </div>
   );
