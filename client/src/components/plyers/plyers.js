bec9b627de8148883db3f0e623830a34dc1454fd
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-20 14:48:42 +0200

    done with player page plus responsive

--- client/src/components/plyers/plyers.js
+++ client/src/components/plyers/plyers.js
@@ -1,5 +1,48 @@
-import React from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../constants/api';
+
+import players from './players.css';
 
 export const Plyers = () => {
-  return <div>plyers</div>;
+  const [playerList, setPlayerList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/playerList`)
+      .then((res) => {
+        if (res) {
+          console.log(res.data);
+          setPlayerList(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err, 'err');
+      });
+  }, []);
+  return (
+    <div className='containerClients'>
+      <div className='wrapperHeaderClients'>
+        <div className='headerClients'>
+          <i class='fas fa-user-edit'></i>Jact Clients
+        </div>
+      </div>
+      {playerList.map((player) => {
+        return (
+          <div className='wrapperPlayer' key={player._id}>
+            <div className='wrapperImg'>
+              <img
+                className='playerImg'
+                src={`${API_URL}/uploads/${player.img}`}
+                alt=''
+              />
+            </div>
+            <div className='wrapperDesc'>
+              <div className='name'>{player.name}</div>
+              <div className='desc'>{player.desc}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
