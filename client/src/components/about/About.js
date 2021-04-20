58dfa0182c59804cd375d4fd15d9490fda80ec4c
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-20 18:10:25 +0200

    about finished and responsivt

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,5 +1,63 @@
 import React from 'react-dom';
+import about from './about.css';
 
 export const About = () => {
-  return <div>hej</div>;
+  return (
+    <div className='aboutContainer'>
+      <div className='aboutGreg'>
+        <h2>About Greg</h2>
+      </div>
+      <div className='wrapperAboutTextGreg'>
+        <div className='arrowLogo'>
+          <i class='fas fa-arrow-right'></i>
+        </div>
+        <div className='text'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            dolore eaque provident, assumenda, dolor exercitationem adipisci
+            vero blanditiis cupiditate officia nostrum, commodi a possimus nobis
+            optio aspernatur architecto? Non, illo libero pariatur placeat
+            consequatur voluptatibus ipsa modi nemo fugiat temporibus nobis.
+            Iusto, sequi est! Quas!
+          </p>
+        </div>
+      </div>
+      <div className='aboutAnders'>
+        <h2>About Anders</h2>
+      </div>
+      <div className='wrapperAboutTextAnders'>
+        <div className='text'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            dolore eaque provident, assumenda, dolor exercitationem adipisci
+            vero blanditiis cupiditate officia nostrum, commodi a possimus nobis
+            optio aspernatur architecto? Non, illo libero pariatur placeat
+            consequatur voluptatibus ipsa modi nemo fugiat temporibus nobis.
+            Iusto, sequi est! Quas!
+          </p>
+        </div>
+        <div className='arrowLogo'>
+          <i class='fas fa-arrow-left'></i>
+        </div>
+      </div>
+      <div className='aboutGreg'>
+        <h2>About Thomson</h2>
+      </div>
+      <div className='wrapperAboutTextGreg'>
+        <div className='arrowLogo'>
+          <i class='fas fa-arrow-right'></i>
+        </div>
+        <div className='text'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            dolore eaque provident, assumenda, dolor exercitationem adipisci
+            vero blanditiis cupiditate officia nostrum, commodi a possimus nobis
+            optio aspernatur architecto? Non, illo libero pariatur placeat
+            consequatur voluptatibus ipsa modi nemo fugiat temporibus nobis.
+            Iusto, sequi est! Quas!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
--- client/src/components/plyers/plyers.js
+++ client/src/components/plyers/plyers.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../constants/api';
-
 import players from './players.css';
 
 export const Plyers = () => {
