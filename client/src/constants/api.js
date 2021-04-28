d44730db0787c6df38b72e6d22a902319350db90
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-28 14:16:01 +0200

    change in client, prod URL

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:4000';
-//export const API_URL =  prod api;
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'http://jact-client.vercel.app';
 
 //vercel ska kunna  autogenerisera  en .env fil
 // set 2 olika miljö variabler i vercel
