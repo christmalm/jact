26b1fb5945edfa2b5bb4345ebcfb5308b91d3763
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-04-28 15:12:39 +0200

    added s in http

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,5 +1,5 @@
 // export const API_URL = 'http://localhost:4000';
-export const API_URL = 'https://jact-client.vercel.app';
+export const API_URL = 'https://jact-server.vercel.app';
 
 //vercel ska kunna  autogenerisera  en .env fil
 // set 2 olika miljö variabler i vercel
